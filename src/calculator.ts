bb87e0b385ad3e7114c8f94526c55b39e1b57b34
LAPTOP-B6C3A7OH\prems <premsuryamj@gmail.com>
2025-01-28 19:50:17 +0530

    Refactor add numbers logic

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,7 +1,10 @@
 export class Calculator {
+    private getNumbers(numbers: string): number[] {
+        const delimeter = ',';
+        return numbers.split(delimeter).map(Number);
+    }
     add(numbers: string): number {
         if (!numbers) return 0;
-        const delimeter = ',';
-        return numbers.split(delimeter).map(Number).reduce((a, b) => a + b, 0);
+        return this.getNumbers(numbers).reduce((a, b) => a + b, 0);
     }
 }
